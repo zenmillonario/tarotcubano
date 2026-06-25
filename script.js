afe9981913cded620c1ab54795b85bef33d632f3
zenmillonario <zenmillonario@gmail.com>
2026-06-25 15:14:14 -0400

    cards

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.15 });
 
 reveals.forEach(el => observer.observe(el));
+
+const slides = document.querySelectorAll('.hero-slide');
+let currentSlide = 0;
+
+setInterval(() => {
+  slides[currentSlide].classList.remove('active');
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.add('active');
+}, 6000);
